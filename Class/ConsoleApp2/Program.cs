81a0ec693d6d77049b035126501b23f32e90abfc
NfolZordo <79935610+NfolZordo@users.noreply.github.com>
2021-11-01 19:27:25 +0200

    01.11.2021.19:26

--- Class/ConsoleApp2/Program.cs
+++ Class/ConsoleApp2/Program.cs
@@ -10,6 +10,430 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            class Notebook
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Computer
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Monitor
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Tablet
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Microphone
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Headphones
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Keyboard
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class ComputerMouse
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Fridge
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Washer
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Dishwasher
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Cookers
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Freezers
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class DryingMachine
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class BuiltInOven
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class BuiltInHob
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class CookerHood
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class CoffeeMachine
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Multicooker
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class MicrowaveOvens
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Blender
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Grill
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class VacuumCleaner
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class RobotVacuumCleaner
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Iron
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class SteamCleaner
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Cupboard
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Curbstone
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Sofa
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
+            class Table
+            {
+                public double height;
+                public double width;
+                public double material;
+                public double weight;
+                public double area;
+                public double getArea(double height, double width)
+                {
+                    area = height * width;
+                    return (area);
+                }
+            }
         }
     }
 }
+
+
+/*
+Ноутбук
+Компьютер 
+Монитор 
+Планшет 
+Микрофон 
+Наушники 
+Клавиатура 
+Компьютерная мышь
+Холодильник
+Стиральная машина
+Посудомоечная машина
+Кухонные плиты
+Морозильные камера
+Сушильная машина
+Встраиваемая духовая шкафа
+Встраиваемая варочная поверхность
+Кухоннаявытяжка
+Кофемашина
+Мультиварка
+Печи СВЧ
+Блендер
+Гриль
+Пылесос
+Робот-пылесос
+Утюг
+Пароочиститель
+Шкаф
+Тумба
+Диван
+Стол
+*/
